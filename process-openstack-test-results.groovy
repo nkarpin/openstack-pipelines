e415254aa1065ad4e86c3b2eae07930f9231967d
Mykyta Karpin <mkarpin@mirantis.com>
2017-08-18 16:55:48 +0300

    Openstack component tests pipelines
    This commit adds three pipelines related to testing
    of MCP openstack components.
    
    - test-openstack-component-pipeline - wrapper pipeline
      for environment deployment and tests launching,
      allows to run tests manually/periodically or by gerrit trigger.
    - run-openstack-tests - launches tempest/stepler tests on deployed
      openstack environment.
    - process-openstack-test-results - processes tests results, recieved
      from upstream job.
    
    Related-Prod: OSCORE-270
    
    Change-Id: I3a0c34de4a00c396054be955822dc3f99485af3a

---
+++ process-openstack-test-results.groovy
@@ -0,0 +1,99 @@
+/**
+ *
+ * Pipeline for test results processing.
+ * Pipeline stages:
+ *  - Downloading of tests results from specified job.
+ *  - Upload results to testrail (Optional).
+ *  - Analysing results and making a decision about build success.
+ *
+ * Expected parameters:
+ *   FAIL_ON_TESTS                Whether to fail build on tests failures or not
+ *   OPENSTACK_VERSION            Version of Openstack being tested
+ *   TARGET_JOB                   Name of the testing job
+ *   TARGET_BUILD_NUMBER          Number of the testing build
+ *   TEST_REPORTER_IMAGE          Docker image for testrail reporter
+ *   TESTRAIL                     Whether upload results to testrail or not
+ *   TEST_MILESTONE               Product version for tests
+ *   TEST_MODEL                   Salt model used in environment
+ *   TEST_SUITE                   Testrail test suite
+ *   TEST_PLAN                    Testrail test plan
+ *   TEST_GROUP                   Testrail test group
+ *   TESTRAIL_QA_CREDENTIALS      Credentials for upload to testrail
+ *   TEST_DATE                    Date of test run
+ *   TEST_PASS_THRESHOLD          Persent of passed tests to consider build successful
+ *
+ */
+
+common = new com.mirantis.mk.Common()
+test = new com.mirantis.mk.Test()
+
+node("docker") {
+    try {
+        testOutputDir = sh(script: "mktemp -d", returnStdout: true).trim()
+
+        //TODO: Implement support for stepler run artifacts
+        stage('Get tests artifacts') {
+            selector = [$class: 'SpecificBuildSelector', buildNumber: "${TARGET_BUILD_NUMBER}"];
+
+            step ([$class: 'CopyArtifact',
+                   projectName: TARGET_JOB,
+                   selector: selector,
+                   filter: '_artifacts/rally_reports.tar',
+                   target: testOutputDir,
+                   flatten: true]);
+
+            dir(testOutputDir) {
+                sh("tar -xf rally_reports.tar")
+            }
+        }
+
+        report = sh(script: "find ${testOutputDir} -name *.xml", returnStdout: true).trim()
+
+        if (TESTRAIL.toBoolean() == true) {
+            stage('Upload tests results to Testrail'){
+
+                def plan = TEST_PLAN ?: "${TEST_MILESTONE}-OSCORE-${TEST_DATE}"
+                def group = TEST_GROUP ?: "${TEST_MODEL}-${OPENSTACK_VERSION}-nightly"
+
+                //ensures that we have up to date image on jenkins slave
+                sh("docker pull ${TEST_REPORTER_IMAGE}")
+
+                test.uploadResultsTestrail(report, TEST_REPORTER_IMAGE, group, TESTRAIL_QA_CREDENTIALS,
+                    plan, TEST_MILESTONE, TEST_SUITE)
+            }
+        }
+
+        //TODO: use xunit publisher plugin to publish results
+        stage('Check tests results'){
+            fileContents = new File(report)
+            parsed = new XmlParser().parse(fileContents)
+            res = parsed['testsuite'][0].attributes()
+
+            failed = res.failures.toInteger()
+            tests = res.tests.toInteger()
+            skipped = res.skipped.toInteger()
+            passed = tests - failed - skipped
+
+            test_info = """
+                        Failed:  ${res.failures}
+                        Errors:  ${res.errors}
+                        Skipped: ${res.skipped}
+                        Tests:   ${res.tests}
+                        """
+            println test_info
+
+            pr = (passed/tests)*100
+
+            if (pr < TEST_PASS_THRESHOLD.toInteger() && FAIL_ON_TESTS){
+                error("${failed} TEMPEST TESTS HAVE FAILED")
+            }
+        }
+    } catch (Throwable e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+        dir(testOutputDir) {
+          deleteDir()
+        }
+    }
+}
---
+++ run-openstack-tests.groovy
@@ -0,0 +1,81 @@
+/**
+ *
+ * Pipeline for tests execution on predeployed Openstack.
+ * Pipeline stages:
+ *  - Launch of tests on deployed environment. Currently
+ *    supports only Tempest tests, support of Stepler
+ *    will be added in future.
+ *  - Archiving of tests results to Jenkins master
+ *  - Processing results stage - triggers build of job
+ *    responsible for results check and upload to testrail
+ *
+ * Expected parameters:
+ *   SALT_MASTER_URL              URL of Salt master
+ *   SALT_MASTER_CREDENTIALS      Credentials to the Salt API
+ *   TEST_TEMPEST_IMAGE           Docker image to run tempest
+ *   TEST_DOCKER_INSTALL          Install docker
+ *   TEST_TEMPEST_TARGET          Salt target to run tempest on
+ *   TEST_TEMPEST_PATTERN         Tempest tests pattern
+ *   TESTRAIL                     Whether upload results to testrail or not
+ *   TEST_MILESTONE               Product version for tests
+ *   TEST_MODEL                   Salt model used in environment
+ *   OPENSTACK_COMPONENT          Name of openstack component being tested
+ *   OPENSTACK_VERSION            Version of Openstack being tested
+ *   PROC_RESULTS_JOB             Name of job for test results processing
+ *   FAIL_ON_TESTS                Whether to fail build on tests failures or not
+ *   TEST_PASS_THRESHOLD          Persent of passed tests to consider build successful
+ *
+ */
+
+common = new com.mirantis.mk.Common()
+git = new com.mirantis.mk.Git()
+salt = new com.mirantis.mk.Salt()
+test = new com.mirantis.mk.Test()
+
+// Define global variables
+def saltMaster
+
+node("python") {
+    try {
+
+        stage ('Connect to salt master') {
+            // Connect to Salt master
+            saltMaster = salt.connection(SALT_MASTER_URL, SALT_MASTER_CREDENTIALS)
+        }
+
+        def log_dir = "/home/rally/rally_reports/${OPENSTACK_COMPONENT}"
+        def reports_dir = "/root/rally_reports/${OPENSTACK_COMPONENT}"
+        def date = sh(script: "date +%Y-%m-%d", returnStdout: true).trim()
+
+        if (common.checkContains('TEST_DOCKER_INSTALL', 'true')) {
+            test.install_docker(saltMaster, TEST_TEMPEST_TARGET)
+        }
+
+        // TODO: implement stepler testing from this pipeline
+        stage('Run OpenStack tests') {
+            test.runTempestTests(saltMaster, TEST_TEMPEST_IMAGE, TEST_TEMPEST_TARGET, TEST_TEMPEST_PATTERN, log_dir)
+        }
+
+        stage('Archive rally artifacts') {
+            test.archiveRallyArtifacts(saltMaster, TEST_TEMPEST_TARGET, reports_dir)
+        }
+
+        stage('Processing results') {
+            build(job: PROC_RESULTS_JOB, parameters: [
+                [$class: 'StringParameterValue', name: 'TARGET_JOB', value: "${env.JOB_NAME}"],
+                [$class: 'StringParameterValue', name: 'TARGET_BUILD_NUMBER', value: "${env.BUILD_NUMBER}"],
+                [$class: 'BooleanParameterValue', name: 'TESTRAIL', value: TESTRAIL.toBoolean()],
+                [$class: 'StringParameterValue', name: 'TEST_MILESTONE', value: TEST_MILESTONE],
+                [$class: 'StringParameterValue', name: 'TEST_MODEL', value: TEST_MODEL],
+                [$class: 'StringParameterValue', name: 'OPENSTACK_VERSION', value: OPENSTACK_VERSION],
+                [$class: 'StringParameterValue', name: 'TEST_DATE', value: date],
+                [$class: 'StringParameterValue', name: 'TEST_PASS_THRESHOLD', value: TEST_PASS_THRESHOLD],
+                [$class: 'BooleanParameterValue', name: 'FAIL_ON_TESTS', value: FAIL_ON_TESTS.toBoolean()]
+            ])
+        }
+
+    } catch (Throwable e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    }
+}
