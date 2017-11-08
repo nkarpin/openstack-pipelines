fd91687fb5c1724274bdf53002b0445ca40403a4
Mykyta Karpin <mkarpin@mirantis.com>
2017-11-08 13:36:15 +0200

    Add pipeline for bandit scanning
    Related-Prod: https://mirantis.jira.com/browse/PROD-15788
    Change-Id: I89a6f7a67bc9684a3970aa12a8f82e41bbe3324e

---
+++ bandit-openstack-pipeline.groovy
@@ -0,0 +1,165 @@
+def gerrit = new com.mirantis.mk.Gerrit()
+def git = new com.mirantis.mk.Git()
+def python = new com.mirantis.mk.Python()
+def common = new com.mirantis.mk.Common()
+
+// TODO: move to pipeline-library
+def runVirtualenvCommandStatus(path, cmd) {
+    def common = new com.mirantis.mk.Common()
+    def res = [:]
+    def stderr = sh(script: 'mktemp', returnStdout: true)
+    def stdout = sh(script: 'mktemp', returnStdout: true)
+
+    try {
+        def virtualenv_cmd = ". ${path}/bin/activate > /dev/null; ${cmd} 1>${stdout} 2>${stderr}"
+        common.infoMsg("[Python ${path}] Run command ${cmd}")
+        def status = sh(script: virtualenv_cmd, returnStatus: true)
+        res['stderr'] = sh(script: "cat ${stderr}", returnStdout: true)
+        res['stdout'] = sh(script: "cat ${stdout}", returnStdout: true)
+        res['status'] = status
+    } finally {
+        sh(script: "rm ${stderr}", returnStdout: true)
+        sh(script: "rm ${stdout}", returnStdout: true)
+    }
+    return res
+}
+
+def runBanditTests(venv, target, excludes='', reportPath='', reportFormat='csv', severity=0, confidence=0) {
+    // Bandit doesn't fail if target path doesn't exist
+    if (!fileExists(target)){
+        error("Target path ${target} doesn't exist, nothing to scan!")
+    }
+    def banditArgs = ["-r ${target}"]
+    if (severity > 0) {
+        banditArgs.add('-' + 'l' * severity)
+    }
+    if (confidence > 0) {
+        banditArgs.add('-' + 'i' * confidence)
+    }
+    if (excludes){
+        banditArgs.add("-x ${excludes}")
+    }
+    if (reportPath){
+        banditArgs.addAll(["-o ${reportPath}", "-f ${reportFormat}"])
+    }
+    def res = runVirtualenvCommandStatus(venv, "bandit ${banditArgs.join(' ')}")
+
+    return res
+}
+
+// there is no native ini parser in groovy
+def getIniParamValue(path, section, param){
+    def parserFile = "${env.WORKSPACE}/iniFileParser.py"
+    def parserScript = """
+import ConfigParser
+import sys
+
+path = sys.argv[1]
+section = sys.argv[2]
+param = sys.argv[3]
+
+config = ConfigParser.ConfigParser()
+config.read([path])
+
+try:
+    val = config.get(section, param)
+except ConfigParser.NoSectionError as e:
+    sys.exit(5)
+sys.stdout.write(val.strip()+'\\n')
+"""
+
+    writeFile file: parserFile, text: parserScript
+
+    def res = [:]
+    def stderr = sh(script: 'mktemp', returnStdout: true)
+    def stdout = sh(script: 'mktemp', returnStdout: true)
+
+    try {
+        def cmd = "python iniFileParser.py ${path} ${section} ${param} 1>${stdout} 2>${stderr}"
+        common.infoMsg("Run command ${cmd}")
+        def status = sh(script: virtualenv_cmd, returnStatus: true)
+        res['stderr'] = sh(script: "cat ${stderr}", returnStdout: true)
+        res['stdout'] = sh(script: "cat ${stdout}", returnStdout: true)
+        res['status'] = status
+    } finally {
+        sh(script: "rm ${stderr}", returnStdout: true)
+        sh(script: "rm ${stdout}", returnStdout: true)
+    }
+
+    return res
+}
+
+node('python'){
+
+    def build_result = 'FAILURE'
+    if (!FAIL_ON_TESTS.toBoolean()){
+        build_result = 'SUCCESS'
+    }
+    def project_url = GERRIT_PROJECT_URL ?: "${GERRIT_SCHEME}://${GERRIT_USER}@${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT}"
+    // get simple project name from GERRIT_PROJECT_URL
+    // (e.g ssh://oscc-ci@review.fuel-infra.org:29418/openstack/nova - nova)
+    def project_name = gerrit._getGerritParamsFromUrl(project_url)[4].tokenize('/').last()
+    def project_path = "${env.WORKSPACE}/${project_name}"
+    def artifacts_dir = '_artifacts'
+    def report_path = "${artifacts_dir}/report-${project_name}.${REPORT_FORMAT}"
+    def venv = "${env.WORKSPACE}/bandit_${project_name}"
+
+    sh("rm -rf ${artifacts_dir} && mkdir -p ${artifacts_dir}")
+
+    stage('Preparing virtual env for bandit'){
+        python.setupVirtualenv(venv, 'python2', ['bandit'], null, true)
+    }
+
+    dir(project_path) {
+        deleteDir()
+        stage('Checkout'){
+            if (common.validInputParam('GERRIT_REFSPEC')){
+                gerrit.gerritPatchsetCheckout(project_url, GERRIT_REFSPEC, GERRIT_BRANCH, CREDENTIALS_ID)
+            } else {
+                git.checkoutGitRepository(project_path, project_url, GERRIT_BRANCH, CREDENTIALS_ID)
+            }
+        }
+
+        stage("Running bandit scan for ${project_name}"){
+
+            def res
+
+            if (UPSTREAM.toBoolean()) {
+                // Currently upstream implementation of bandit doesn't generate report
+                def ini_res = getIniParamValue('tox.ini', 'testenv:bandit', 'commands')[0].trim()
+
+                if (ini_res['status'] == 0){
+                    res = runVirtualenvCommandStatus(venv, "${ini_res['stdout']} -o ${env.WORKSPACE}/${report_path} -f ${REPORT_FORMAT}")
+                } else if (ini_res['status'] == 5){
+                    common.errorMsg('Bandit tests not found in project\'s tox.ini, skipping upstream tests')
+                    currentBuild.result = 'NOT_BUILT'
+                    return
+                } else {
+                    error("Failed to get bandit command\n${ini_res['stderr']}")
+                }
+            } else {
+                // Not all projects have bandit in tox.ini
+                def code_dir = getIniParamValue('setup.cfg', 'files', 'packages')[0]
+                res = runBanditTests(venv, "${code_dir}", 'tests', "${env.WORKSPACE}/${report_path}",
+                    REPORT_FORMAT, SEVERITY.toInteger(), CONFIDENCE.toInteger())
+            }
+
+            if (res['status'] == 1){
+                common.errorMsg('''
+                    -------------------------------------------------------------
+                    !!! Security violations found, please check bandit report !!!
+                    -------------------------------------------------------------
+                    '''
+                )
+                currentBuild.result = build_result
+            } else if (res['status'] > 1){
+                common.errorMsg("Bandit tests failed:\n ${res[stderr]}")
+                currentBuild.result = 'FAILURE'
+            }
+        }
+    }
+
+    stage("Archieving bandit scan results for ${project_name}"){
+        archiveArtifacts artifacts: report_path
+    }
+}
