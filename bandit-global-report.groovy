5ffcbc1f0a4c7c6623869ea1667f181765c72a0a
Mykyta Karpin <mkarpin@mirantis.com>
2017-12-04 16:55:27 +0200

    Different fixes
    - tar filename now has version
    - gerrit url is fixed
    
    Change-Id: I082e76e991a821e0e6f825d0c2ef1d19ec8c5034

--- bandit-global-report.groovy
+++ bandit-global-report.groovy
@@ -9,36 +9,60 @@ project_list = ['nova', 'cinder', 'ironic', 'keystone', 'keystonemiddleware', 'k
                 'python-barbicanclient', 'python-designateclient', 'python-monascaclient'
                 ]
 
-def project_bandit_test = [:]
+def concurrency = REPORT_CONCURRENCY.toInteger()
 def builds = [:]
-for (int i = 0; i < project_list.size(); i++) {
-    def project_name = project_list[i]
 
-    project_bandit_test["Bandit test: ${project_name}"] = {
-        node('python'){
-            //url = "ssh://oscc-ci@review.fuel-infra.org:29418/openstack/${project_name}"
-            builds["${project_name}"] = build job: "oscore-bandit-${TYPE}-${project_name}", propagate: false, parameters: [
-                [$class: 'StringParameterValue', name: 'SEVERITY', value: SEVERITY],
-                [$class: 'StringParameterValue', name: 'CONFIDENCE', value: CONFIDENCE],
-                [$class: 'StringParameterValue', name: 'REPORT_FORMAT', value: REPORT_FORMAT],
-                [$class: 'StringParameterValue', name: 'GERRIT_BRANCH', value: GERRIT_BRANCH],
-                [$class: 'StringParameterValue', name: 'CREDENTIALS_ID', value: CREDENTIALS_ID],
-                [$class: 'BooleanParameterValue', name: 'FAIL_ON_TESTS', value: false],
-                [$class: 'StringParameterValue', name: 'GERRIT_PROJECT_URL', value: "${GERRIT_URL}/${project_name}"],
-            ]
+def run_parallel_scans(project_list=[], builds=[:]) {
+    def project_bandit_test = [:]
+    for (int i = 0; i < project_list.size(); i++) {
+        def project_name = project_list[i]
+        project_bandit_test["Bandit test: ${project_name}"] = {
+            node('python'){
+                //url = "ssh://oscc-ci@review.fuel-infra.org:29418/openstack/${project_name}"
+                builds["${project_name}"] = build job: "oscore-bandit-${TYPE}-${project_name}", propagate: false, parameters: [
+                    [$class: 'StringParameterValue', name: 'SEVERITY', value: SEVERITY],
+                    [$class: 'StringParameterValue', name: 'CONFIDENCE', value: CONFIDENCE],
+                    [$class: 'StringParameterValue', name: 'REPORT_FORMAT', value: REPORT_FORMAT],
+                    [$class: 'StringParameterValue', name: 'GERRIT_BRANCH', value: GERRIT_BRANCH],
+                    [$class: 'StringParameterValue', name: 'CREDENTIALS_ID', value: CREDENTIALS_ID],
+                    [$class: 'BooleanParameterValue', name: 'FAIL_ON_TESTS', value: false],
+                    [$class: 'StringParameterValue', name: 'GERRIT_PROJECT_URL', value: "${GERRIT_URL}${project_name}"],
+                ]
+            }
         }
     }
+    parallel project_bandit_test
 }
 
 stage('Running parallel Bandit tests') {
-    parallel project_bandit_test
+
+    int num = project_list.size()
+    int mod = num % concurrency
+    def start
+    def end
+    for (j = 0; j <= num - concurrency; j = j + concurrency) {
+         start = j
+         end = j + concurrency - 1
+         println "${start},${end}"
+         println project_list[start..end]
+         run_parallel_scans(project_list[start..end], builds)
+    }
+
+    if (mod > 0) {
+        start = num - mod
+        end = num - 1
+        println "${start},${end}"
+        println project_list[start..end]
+        run_parallel_scans(project_list[start..end], builds)
+    }
+
 }
 
 node('python') {
     def reports_dir = "${env.WORKSPACE}/reports/"
     def artifacts_dir = '_artifacts/'
-    def output = 'reports.tar'
     def version = GERRIT_BRANCH.replaceAll('/','_')
+    def output = "reports_${version}.tar"
     sh("rm -rf ${reports_dir} && mkdir -p ${reports_dir}")
 
     stage('Getting test reports') {
@@ -47,7 +71,7 @@ node('python') {
             def selector = [$class: 'SpecificBuildSelector', buildNumber: "${number}"]
             if(builds[k].result == 'SUCCESS') {
                 step ([$class: 'CopyArtifact',
-                       projectName: "oscore-bandit-${TYPE}-${project_name}",
+                       projectName: "oscore-bandit-${TYPE}-${k}",
                        selector: selector,
                        filter: "_artifacts/report-${k}.${REPORT_FORMAT}",
                        target: "${reports_dir}",
@@ -56,7 +80,7 @@ node('python') {
         }
     }
 
-    if (REPORT_FORMAT == 'csv'){
+    if (REPORT_FORMAT == 'json'){
         stage('Creating global report') {
             def global_list = []
             dir("${reports_dir}") {
@@ -64,11 +88,7 @@ node('python') {
                 for (f in report_list){
                     def title = f-'report-'-'.csv'
                     def r = readFile(f)
-                    if (r.tokenize('\n').size() == 1) {
-                        global_list.add(title+'\n'+'no issues found\n')
-                    } else {
-                        global_list.add(title+'\n'+r)
-                    }
+                    global_list.add(title+'\n'+r)
                 }
             }
             writeFile file: "${reports_dir}/bandit_report_${version}.csv", text: global_list.join('\n')
